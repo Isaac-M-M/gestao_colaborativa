cefa066fc621c7730deb0fd7484e6a93dec16536
Isaac-M-M <isaacmesquitam6@gmail.com>
2025-05-18 01:09:08 -0300

    Alteração na cor do botão Salvar no formulário de projeto

--- gestao_colaborativa/urls.py
+++ gestao_colaborativa/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from core.views import ProjetoViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'api/projetos', ProjetoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # suas rotas normais
-    path('api/', include('core.api_urls')),  # rotas da API REST
+    path('', include('core.urls')),
+    path('', include(router.urls)),
 ]
